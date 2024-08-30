b84446db08bc9638d732616352d49dd52f1cdc76
eduard-lab-front-end <eduard-lab@web.de>
2024-08-30 16:33:26 +0200

    done

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,10 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+      it("should return undefined if any of the two arguments is not a number", () => {
+        expect(add('2',2)).toBe(undefined)
+        expect(add(2,'2')).toBe(undefined)
+      })
     });
   });
   
